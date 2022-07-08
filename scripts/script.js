4f47cd4a79553addbd19d60aa82aa192f6e578a3
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-08 16:24:59 +0100

    displaying mines and numbers

---
+++ scripts/script.js
@@ -0,0 +1,78 @@
+tileGrid = document.querySelector(".tile-grid");
+
+for (let i = 0; i < 81; i++) {
+  tileGrid.innerHTML += `<div class="tile" id="id${i + 1}"></div>`;
+}
+
+const tiles = document.querySelectorAll(".tile");
+
+const clearAll = () => {
+  tiles.forEach((tile) => {
+    tile.innerHTML = "";
+  });
+};
+
+const positionMines = () => {
+  const mineLocations = [];
+
+  for (let i = 0; mineLocations.length < 10; i++) {
+    console.log(mineLocations.length);
+    let randomNum = Math.floor(Math.random() * 81 + 1);
+    if (!mineLocations.includes(randomNum)) {
+      mineLocations.push(randomNum);
+    }
+  }
+
+  mineLocations.forEach((location) => {
+    let matchingTile = document.querySelector(`#id${location}`);
+    matchingTile.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
+  });
+};
+
+const displayNumbers = () => {
+  tiles.forEach((tile) => {
+    if (!tile.innerHTML) {
+      let adjacentBombs = 0;
+      const tileIdNo = Number(tile.id.substring(2));
+      const tileDistances = !(tileIdNo % 9)
+        ? [-10, -9, -1, 8, 9]
+        : tileIdNo % 9 === 1
+        ? [-9, -8, 1, 9, 10]
+        : [-10, -9, -8, -1, 1, 8, 9, 10];
+      tileDistances.forEach((distance) => {
+        if (tileIdNo + distance > 0 && tileIdNo + distance < 82) {
+          let checkedForMines = document.querySelector(
+            `#id${tileIdNo + distance}`,
+          );
+          if (
+            checkedForMines.innerHTML === `<i class="fa-solid fa-bomb"></i>`
+          ) {
+            adjacentBombs++;
+          }
+        }
+      });
+      tile.innerHTML = adjacentBombs ? adjacentBombs : "";
+      if (tile.innerHTML === "1") {
+        tile.style.color = "blue";
+      } else if (tile.innerHTML === "2") {
+        tile.style.color = "green";
+      } else if (tile.innerHTML === "3") {
+        tile.style.color = "red";
+      } else if (tile.innerHTML === "4") {
+        tile.style.color = "purple";
+      } else if (tile.innerHTML === "5") {
+        tile.style.color = "orange";
+      } else if (tile.innerHTML === "6") {
+        tile.style.color = "pink";
+      }
+    }
+  });
+};
+
+const startNewGame = () => {
+  clearAll();
+  positionMines();
+  displayNumbers();
+};
+
+startNewGame();
